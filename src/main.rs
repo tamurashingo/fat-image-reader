a4088032d5e84c0862fa9b1a443e891eeb304f12
tamura shingo <tamura.shingo@gmail.com>
2023-05-09 08:01:06 +0900

    add BootSector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+#[repr(C, packed)]
+#[derive(Debug, Copy, Clone)]
+struct BootSector {
+    BS_JmpBoot: [u8; 3],
+    BS_OEMName: [u8; 8],
+    BPB_BytsPerSec: [u8; 2],
+    BPB_SecPerClus: [u8; 1],
+    BPB_RsvdSecCnt: [u8; 2],
+    BPB_NumFATs: [u8; 1],
+    BPB_RootEntCnt: [u8; 2],
+    BPB_TotSec16: [u8; 2],
+    BPB_Media: [u8; 1],
+    BPB_FATSz16: [u8; 2],
+    BPB_SecPerTrk: [u8; 2],
+    BPB_NumHeads: [u8; 2],
+    BPB_HiddSec: [u8; 4],
+    BPB_TotSec32: [u8; 4],
+
+
+    BS_DrvNum: [u8; 1],
+    BS_Reserved: [u8; 1],
+    BS_BootSig: [u8; 1],
+    BS_VolID: [u8; 4],
+    BS_VolLab: [u8; 11],
+    BS_FilSysType: [u8; 8],
+    BS_BootCode: [u8; 448],
+    BS_BootSign: [u8; 2],
 }
+
+
+
+
+fn main() {
+    let bytes = std::fs::read("data.bin").unwrap();
+    println!("{}", bytes.len());
+
+    let (head, body, _tail) = unsafe { bytes.align_to::<BootSector>() };
+    let boot_sector = &body[0];
+
+    println!("{:?}", boot_sector);
+}
+
