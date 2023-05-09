35259ebba18c4cb7f85b22a99449b5d4bc26b05c
tamura shingo <tamura.shingo@gmail.com>
2023-05-10 06:05:39 +0900

    fatモジュールにあわせてmainを変更

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod bootsector;
+mod fat;
 
 fn main() {
     let image = std::fs::read("data.bin").unwrap();
@@ -6,6 +7,13 @@ fn main() {
 
     println!("oem_name:{}", bootsector::oem_name(&image));
     println!("total_sectors:{}", bootsector::total_sectors(&image));
+
+    let fat = fat::Region::new(&image);
+    println!("type:{}", fat.fat_type);
+
+    println!("dir:{}", fat.root_dir_start_sector);
+
+    println!("{:?}", &image[((fat.root_dir_start_sector * fat.sector_size) as usize)..]);
 
 }
 
