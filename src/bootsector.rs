aef58b6b8048de47106387ae1b65d1a1c88b8277
tamura shingo <tamura.shingo@gmail.com>
2023-05-09 22:52:11 +0900

    add bootsector module

---
+++ src/bootsector.rs
@@ -0,0 +1,63 @@
+pub fn oem_name(image: &Vec<u8>) -> &str {
+  let mem = &image[3..9];
+  std::str::from_utf8(mem).unwrap()
+}
+
+pub fn bytes_per_sector(image: &Vec<u8>) -> u32 {
+  (image[0x00B] as u32)
+  + ((image[0x00B+1] as u32)  << 8)
+}
+
+pub fn sectors_per_cluster(image: &Vec<u8>) -> u32 {
+  image[0x00D] as u32
+}
+
+pub fn reserved_sectors(image: &Vec<u8>) -> u32 {
+  (image[0x00E] as u32)
+  + ((image[0x00E+1] as u32) << 8)
+}
+
+pub fn num_of_fats(image: &Vec<u8>) -> u32 {
+  // almost always 2
+  image[0x010] as u32
+}
+
+pub fn num_of_root_directory_entries(image: &Vec<u8>) -> u32 {
+  (image[0x011] as u32)
+  + ((image[0x011+1] as u32) << 8)
+}
+
+pub fn total_sectors(image: &Vec<u8>) -> u32 {
+  let f16 = (image[0x013] as u32) + ((image[0x013+1] as u32) << 8);
+  if f16 != 0 {
+    f16
+  } else {
+    (image[0x020] as u32)
+    + ((image[0x020+1] as u32) << 8)
+    + ((image[0x020+2] as u32) << 16)
+    + ((image[0x020+3] as u32) << 24)
+  }
+}
+
+pub fn media_descriptor(image: &Vec<u8>) -> u32 {
+  image[0x015] as u32
+}
+
+pub fn sectors_per_fats(image: &Vec<u8>) -> u32 {
+  let f16 = (image[0x016] as u32) + ((image[0x016] as u32) << 8);
+  if f16 != 0 {
+    f16
+  } else {
+    (image[0x024] as u32)
+    + ((image[0x024+1] as u32) << 8)
+    + ((image[0x024+2] as u32) << 16)
+    + ((image[0x024+3] as u32) << 24)
+  }
+}
+
+fn slice_reserved_sectors(image: &Vec<u8>) -> &[u8] {
+  let sector_size = bytes_per_sector(image) as usize;
+  let reserved_sector_count = reserved_sectors(image) as usize;
+
+  &image[0..=(sector_size * reserved_sector_count)]
+}
