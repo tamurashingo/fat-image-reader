0f08e2d7422701e3144d66e56c988eae44a0d3a7
tamura shingo <tamura.shingo@gmail.com>
2023-05-14 08:51:21 +0900

    最初のセクタ情報を構造体で定義

--- src/bootsector.rs
+++ src/bootsector.rs
@@ -14,70 +14,70 @@ pub fn read_first_sector(f: &mut File) -> Result<FirstSector, Error> {
   Ok(FirstSector { image: sector })
 }
 
-pub fn oem_name(image: &Vec<u8>) -> &str {
-  let mem = &image[3..9];
+pub fn oem_name(sector: &FirstSector) -> &str {
+  let mem = &sector.image[3..9];
   std::str::from_utf8(mem).unwrap()
 }
 
-pub fn bytes_per_sector(image: &Vec<u8>) -> u32 {
-  (image[0x00B] as u32)
-  + ((image[0x00B+1] as u32)  << 8)
+pub fn bytes_per_sector(sector: &FirstSector) -> u32 {
+  (sector.image[0x00B] as u32)
+  + ((sector.image[0x00B+1] as u32)  << 8)
 }
 
-pub fn sectors_per_cluster(image: &Vec<u8>) -> u32 {
-  image[0x00D] as u32
+pub fn sectors_per_cluster(sector: &FirstSector) -> u32 {
+  sector.image[0x00D] as u32
 }
 
-pub fn reserved_sectors(image: &Vec<u8>) -> u32 {
-  (image[0x00E] as u32)
-  + ((image[0x00E+1] as u32) << 8)
+pub fn reserved_sectors(sector: &FirstSector) -> u32 {
+  (sector.image[0x00E] as u32)
+  + ((sector.image[0x00E+1] as u32) << 8)
 }
 
-pub fn num_of_fats(image: &Vec<u8>) -> u32 {
+pub fn num_of_fats(sector: &FirstSector) -> u32 {
   // almost always 2
-  image[0x010] as u32
+  sector.image[0x010] as u32
 }
 
-pub fn num_of_root_directory_entries(image: &Vec<u8>) -> u32 {
-  (image[0x011] as u32)
-  + ((image[0x011+1] as u32) << 8)
+pub fn num_of_root_directory_entries(sector: &FirstSector) -> u32 {
+  (sector.image[0x011] as u32)
+  + ((sector.image[0x011+1] as u32) << 8)
 }
 
-pub fn total_sectors(image: &Vec<u8>) -> u32 {
-  let f16 = (image[0x013] as u32) + ((image[0x013+1] as u32) << 8);
+pub fn total_sectors(sector: &FirstSector) -> u32 {
+  let f16 = (sector.image[0x013] as u32) + ((sector.image[0x013+1] as u32) << 8);
   if f16 != 0 {
     f16
   } else {
-    (image[0x020] as u32)
-    + ((image[0x020+1] as u32) << 8)
-    + ((image[0x020+2] as u32) << 16)
-    + ((image[0x020+3] as u32) << 24)
+    (sector.image[0x020] as u32)
+    + ((sector.image[0x020+1] as u32) << 8)
+    + ((sector.image[0x020+2] as u32) << 16)
+    + ((sector.image[0x020+3] as u32) << 24)
   }
 }
 
-pub fn media_descriptor(image: &Vec<u8>) -> u32 {
-  image[0x015] as u32
+pub fn media_descriptor(sector: &FirstSector) -> u32 {
+  sector.image[0x015] as u32
 }
 
-pub fn sectors_per_fats(image: &Vec<u8>) -> u32 {
-  let f16 = (image[0x016] as u32) + ((image[0x016] as u32) << 8);
+pub fn sectors_per_fats(sector: &FirstSector) -> u32 {
+  let f16 = (sector.image[0x016] as u32) + ((sector.image[0x016] as u32) << 8);
   if f16 != 0 {
     f16
   } else {
-    (image[0x024] as u32)
-    + ((image[0x024+1] as u32) << 8)
-    + ((image[0x024+2] as u32) << 16)
-    + ((image[0x024+3] as u32) << 24)
+    (sector.image[0x024] as u32)
+    + ((sector.image[0x024+1] as u32) << 8)
+    + ((sector.image[0x024+2] as u32) << 16)
+    + ((sector.image[0x024+3] as u32) << 24)
   }
 }
 
-fn slice_reserved_sectors(image: &Vec<u8>) -> &[u8] {
-  let sector_size = bytes_per_sector(image) as usize;
-  let reserved_sector_count = reserved_sectors(image) as usize;
+fn slice_reserved_sectors(sector: &FirstSector) -> &[u8] {
+  let sector_size = bytes_per_sector(sector) as usize;
+  let reserved_sector_count = reserved_sectors(sector) as usize;
 
-  &image[0..=(sector_size * reserved_sector_count)]
+  &sector.image[0..=(sector_size * reserved_sector_count)]
 }
 
-pub fn reserved_sector_size(image: &Vec<u8>) -> u32 {
-  bytes_per_sector(image) * reserved_sectors(image)
+pub fn reserved_sector_size(sector: &FirstSector) -> u32 {
+  bytes_per_sector(sector) * reserved_sectors(sector)
 }
--- src/fat.rs
+++ src/fat.rs
@@ -43,15 +43,15 @@ fn fat_type(data_sector_size: u32, sectors_per_cluster: u32) -> FatType {
 
 
 impl Region {
-  pub fn new(image: &Vec<u8>) -> Self {
-    let fat_start_sector = bootsector::reserved_sectors(image);
-    let fat_sector_size = bootsector::sectors_per_fats(image) * bootsector::num_of_fats(image);
+  pub fn new(sector: &bootsector::FirstSector) -> Self {
+    let fat_start_sector = bootsector::reserved_sectors(sector);
+    let fat_sector_size = bootsector::sectors_per_fats(sector) * bootsector::num_of_fats(sector);
     let root_dir_start_sector = fat_start_sector + fat_sector_size;
-    let root_dir_sector_size = (32 * bootsector::num_of_root_directory_entries(image) + bootsector::bytes_per_sector(image) - 1) / bootsector::bytes_per_sector(image);
+    let root_dir_sector_size = (32 * bootsector::num_of_root_directory_entries(sector) + bootsector::bytes_per_sector(sector) - 1) / bootsector::bytes_per_sector(sector);
     let data_start_sector = root_dir_start_sector + root_dir_sector_size;
-    let data_sector_size = bootsector::total_sectors(image) - data_start_sector;
-    let fat_type = fat_type(data_sector_size, bootsector::sectors_per_cluster(image));
-    let sector_size = bootsector::bytes_per_sector(image);
+    let data_sector_size = bootsector::total_sectors(sector) - data_start_sector;
+    let fat_type = fat_type(data_sector_size, bootsector::sectors_per_cluster(sector));
+    let sector_size = bootsector::bytes_per_sector(sector);
 
     Self {
       fat_start_sector,
